8aaa5371a195aca5938ad04bbc459b37cbd437dd
wenzheng <wz7014@163.com>
2022-01-10 15:32:28 +0800

    chunck

--- lodash/dwen_zheng-lodash.js
+++ lodash/dwen_zheng-lodash.js
@@ -1,6 +1,42 @@
 var  dwen_zheng = {
   chunk: function(array, size) {
-
+    if (array.length <= size) return array
+    let result = []
+    let temp = []
+    for (let i = 0; i < array.length; i++) {
+      temp.push(array[i])
+      if (temp.length == size) {
+        result.push(temp)
+      }
+      temp = []
+    }
+    return result
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
